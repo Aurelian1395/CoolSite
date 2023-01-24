d4ecbb375892358ec788424564b45b0482f1ec30
Silvano <silvano.norberti@btinkeeng.com>
2023-01-24 17:32:43 +0100

    Missed one edit about index.js

--- Index.js
+++ Index.js
@@ -22,20 +22,39 @@ function submit() {
   document.getElementById('valore').innerHTML = password
 }
 
-console.log('0');
 
+// ======================================================
+
+// Prima di eseguire questa parte devi scaricare il mio progetto 
+
+// Installare node.js https://nodejs.org/it/download/ 
+// Nella powershell o cmd scrivi: git clone https://github.com/Silvano14/Fastify_server.git (Nella cartella dove metti i progetti)
+// Entra nel mio progetto e scrivi nella console:  npm install 
+// Dopo che è finito:  node server.js 
+
+
+console.log('0 prima del fetch');
+
+// http è il protocollo utilizzato, di solito per la richiesta di dati, https è quello "sicuro"
+// 127.0.0.1 signifca che stai interrogando l'ip della tua stessa macchina
+// il 3000 è la porta alla quale la istanza mette a disposizione la comunicazione
 fetch('http://127.0.0.1:3000')
+  // Uso questo then per formattare la risposta del server in un dato leggibile
+  //https://developer.mozilla.org/en-US/docs/Web/API/Response/json
   .then(function (response) {
     return response.json();
   })
+  //Qui leggo la risposta del server e mostro in console il campo data che, di solito, è il campo con i valori
   .then(function (jsonResponse) {
     console.log('2 Dal server ricevo: ', jsonResponse.data)
   })
-  .catch(function () {
+  .catch(function (reject) {
+    console.log("Errore durante la richiesta: ", reject)
     // Qui entro se riscontro qualche errore durante la richiesta 
   })
   .finally(function () {
-    // Qui entro OGNI volta che la richiesta finisce (sia in bene che in male)
+    console.log('3 Vibranium finally!')
+    // Qui entro SEMPRE, ogni volta che faccio una richiesta
   })
 
 // Questa console verrà eseguita subito, quando il fetch avrà ricevuto i dati farà il console.log nel then
