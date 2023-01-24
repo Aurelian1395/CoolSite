47b705a7a2d1f71c53532fbbefe0d8b4f195d6e4
Silvano <silvano.norberti@btinkeeng.com>
2023-01-24 16:12:58 +0100

    Put fetch and some explanation

--- Index.js
+++ Index.js
@@ -5,31 +5,40 @@ const passwordInput = document.getElementById("password")
 
 usernameInput.addEventListener('input', setUsername)
 let username = "";
-function setUsername(e) {
 
-    username = e.target.value
+function setUsername(e) {
+  username = e.target.value
 }
 
 passwordInput.addEventListener('input', setPassword)
-let password=""
+let password = ""
 
 function setPassword(e) {
-    password = e.target.value
-
+  password = e.target.value
 }
 
 
 function submit() {
-    document.getElementById('valore').innerHTML= password
-
-        
-
-         
+  document.getElementById('valore').innerHTML = password
 }
 
+console.log('0');
 
+fetch('http://127.0.0.1:3000')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonResponse) {
+    console.log('2 Dal server ricevo: ', jsonResponse.data)
+  })
+  .catch(function () {
+    // Qui entro se riscontro qualche errore durante la richiesta 
+  })
+  .finally(function () {
+    // Qui entro OGNI volta che la richiesta finisce (sia in bene che in male)
+  })
 
-
-
-//fetch
+// Questa console verrà eseguita subito, quando il fetch avrà ricevuto i dati farà il console.log nel then
+// Te l'ho messo per farti capire l'ordine con cui funzionano 
+console.log('1 dopo il fetch')
 
