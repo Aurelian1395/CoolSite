7bb1fdaa45cb5528b18c533d1124e276f50c7c03
Aure <turduf3@gmail.com>
2023-05-31 06:54:14 +0200

    Fetch call

--- Index.js
+++ Index.js
@@ -1,83 +1,61 @@
 "use strict";
 
-const usernameInput = document.getElementById("username")
-const passwordInput = document.getElementById("password")
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
 
-usernameInput.addEventListener('input', setUsername)
+usernameInput.addEventListener("input", setUsername);
 let username = "";
 
 function setUsername(e) {
-  username = e.target.value
+  username = e.target.value;
 }
 
-passwordInput.addEventListener('input', setPassword)
-let password = ""
+passwordInput.addEventListener("input", setPassword);
+let password = "";
 
 function setPassword(e) {
-  password = e.target.value
+  password = e.target.value;
 }
+// Chiamata al server
 
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return response;
+}
 
 function submit() {
+  var password = document.getElementById("password").value;
+  var username = document.getElementById("username").value;
+  let credenziali = {
+    username: username,
+    password: password,
+  };
+  if (!username) {
+    alert("Devi inserire un username");
+    return;
+  }
 
-  var password = document.getElementById('password').value;
-  var username = document.getElementById('username').value;
+  if (password == "" || password == undefined) {
+    alert("devi inserire una password corretta");
+    return;
+  }
 
-
-if((username == "")||(username== undefined) ) {
-  alert("Devi inserire un username");
- 
-
-} 
-if((password== "") || (password)== undefined){
-  alert( "devi inserire una password corretta")
+  postData("http://localhost:3000/login", credenziali)
+    .then((response) => {
+      if (!response.ok) {
+        return Promise.reject(response);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Success");
+      console.log(data);
+    });
 }
-
-else{
-  alert( "tutti i campi sono compilati correttamente")
-}
-
-
-
-
-}
-
-
-
-// ======================================================
-
-// Prima di eseguire questa parte devi scaricare il mio progetto 
-
-// Installare node.js https://nodejs.org/it/download/ 
-// Nella powershell o cmd scrivi: git clone https://github.com/Silvano14/Fastify_server.git (Nella cartella dove metti i progetti)
-// Entra nel mio progetto e scrivi nella console:  npm install 
-// Dopo che è finito:  node server.js 
-
-
-console.log('0 prima del fetch');
-
-// http è il protocollo utilizzato, di solito per la richiesta di dati, https è quello "sicuro"
-// 127.0.0.1 signifca che stai interrogando l'ip della tua stessa macchina
-// il 3000 è la porta alla quale la istanza mette a disposizione la comunicazione
-fetch('http://127.0.0.1:3000/')
-  // Uso questo then per formattare la risposta del server in un dato leggibile
-  //https://developer.mozilla.org/en-US/docs/Web/API/Response/json
-  .then(function (response) {
-    return response.json();
-  })
-  //Qui leggo la risposta del server e mostro in console il campo data che, di solito, è il campo con i valori
-  .then(function (jsonResponse) {
-    console.log('2 Dal server ricevo: ', jsonResponse.data)
-  })
-  .catch(function (reject) {
-    console.log("Errore durante la richiesta: ", reject)
-    // Qui entro se riscontro qualche errore durante la richiesta 
-  })
-  .finally(function () {
-    console.log('3 Vibranium finally!')
-    // Qui entro SEMPRE, ogni volta che faccio una richiesta
-  })
-
-// Questa console verrà eseguita subito, quando il fetch avrà ricevuto i dati farà il console.log nel then
-// Te l'ho messo per farti capire l'ordine con cui funzionano 
-console.log('1 dopo il fetch')
